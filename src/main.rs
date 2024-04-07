aa4dd036562851d1bc625f0431498deb9a71a6a0
SebaRenner <sebastian.renner@gmx.ch>
2024-04-07 17:16:55 +0200

    Read out cpu information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use sysinfo::System;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = System::new();
+
+    sys.refresh_all();
+
+    let cpus = sys.cpus();
+
+    println!("CPU");
+    println!("Model:                {}", cpus[0].brand());
+    match sys.physical_core_count() {
+        Some(count) => println!("Number of Cores:      {}", count),
+        None => println!("Unable to retrieve cores"),
+    }
+    println!("Number of CPUs:       {}", cpus.len());
 }
